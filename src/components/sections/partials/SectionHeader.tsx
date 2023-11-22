bc25d7f1afb98b3f21fb1a27c6b03946f46449b7
Christian Pedersen <chris@buzzkill.dev>
2023-11-22 14:26:16 +0100

    prepare section header structure Co-authored-by: Cathleen Wilan Møller <cathleen5068@gmail.com>

---
+++ src/components/sections/partials/SectionHeader.tsx
@@ -0,0 +1,15 @@
+interface SectionHeaderProps {
+    subheading: string
+    heading: string
+    description: string
+}
+
+export default function SectionHeader({subheading, heading, description}: SectionHeaderProps) {
+    return (
+        <header className={"grid gap-4"}>
+            <h3 className={"text-2xl"}>{subheading}</h3>
+            <h2 className={"text-5xl"}>{heading}</h2>
+            <p>{description}</p>
+        </header>
+    )
+}
