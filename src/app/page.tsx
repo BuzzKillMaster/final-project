a16ea78a05597d344f89dfd40c1cc63efa836521
Christian Pedersen <chris@buzzkill.dev>
2023-11-22 14:27:20 +0100

    import and display components on home page Co-authored-by: Vicky Sonne Thomsen <vst1996@live.dk>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
+import AppHeader from "@/components/app/AppHeader";
+import AppFooter from "@/components/app/AppFooter";
+import HeroSection from "@/components/sections/HeroSection";
+import TrainingSection from "@/components/sections/TrainingSection";
+import ProfileSection from "@/components/sections/ProfileSection";
+import QualificationsSection from "@/components/sections/QualificationsSection";
+import ApproachSection from "@/components/sections/ApproachSection";
+import TestimonialsSection from "@/components/sections/TestimonialsSection";
+import GuideSection from "@/components/sections/GuideSection";
+import ContactSection from "@/components/sections/ContactSection";
+
 export default function Home() {
     return (
-        <main></main>
+        <>
+            <AppHeader/>
+
+            <main>
+                <HeroSection/>
+                <TrainingSection/>
+                <ProfileSection/>
+                <QualificationsSection/>
+                <ApproachSection/>
+                <TestimonialsSection/>
+                <GuideSection/>
+                <ContactSection/>
+            </main>
+
+            <AppFooter/>
+        </>
     )
 }
