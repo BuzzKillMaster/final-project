e1f5c4b04b6636484af8daf33695b73c954209c5
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 08:00:47 +0100

    introduce qualifications section

--- src/components/sections/QualificationsSection.tsx
+++ src/components/sections/QualificationsSection.tsx
@@ -1,3 +1,32 @@
+import SectionContainer from "@/components/sections/partials/SectionContainer";
+import SectionHeader from "@/components/sections/partials/SectionHeader";
+import FakeDynamicCard from "@/components/FakeDynamicCard";
+
+const data = Array(3).fill({
+    title: "Lorem ipsum dolor sit amet",
+    description: "Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta.",
+    imageUri: "https://fakeimg.pl/500/",
+    imageAlt: "temp",
+})
+
 export default function QualificationsSection() {
-    return (<></>)
+    return (
+        <SectionContainer containerClassName={"grid grid-cols-2 gap-20 items-center"}>
+            <SectionHeader subheading={"Lorem ipsum"} heading={"Lorem ipsum dolor sit amet"} description={"Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta."} />
+
+            <div className={"grid gap-12 mt-20"}>
+                {data.map((item, index) => (
+                    <FakeDynamicCard
+                        key={index}
+                        title={item.title}
+                        description={item.description}
+                        imageUri={item.imageUri}
+                        imageAlt={item.imageAlt}
+                        imageHeight={100}
+                        imageWidth={100}
+                    />
+                ))}
+            </div>
+        </SectionContainer>
+    )
 }
