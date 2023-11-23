81ee00a9a0b3cd0208a7331639ea14e6b4fdad2f
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 08:50:46 +0100

    add contact section without map

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,3 +1,17 @@
+import SectionContainer from "@/components/sections/partials/SectionContainer";
+import SectionHeader from "@/components/sections/partials/SectionHeader";
+import ContactForm from "@/components/sections/contact/ContactForm";
+
 export default function ContactSection() {
-    return (<></>)
+    return (
+        <SectionContainer containerClassName={"grid grid-cols-2 gap-20 items-center"}>
+            <SectionHeader
+                subheading={"Lorem ipsum"}
+                heading={"Lorem ipsum dolor sit amet"}
+                description={"Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta."}
+            >
+                <ContactForm />
+            </SectionHeader>
+        </SectionContainer>
+    )
 }
