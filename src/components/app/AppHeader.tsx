cbdb5dc24928c5bc9aaa682142d491a635de9264
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 09:43:29 +0100

    add sticky header

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -1,3 +1,13 @@
+import AppNavigation from "@/components/app/navigation/AppNavigation";
+
 export default function AppHeader() {
-    return (<></>)
+    return (
+        <header className={"p-12 bg-white sticky top-0 z-50"}>
+            <div className={"container mx-auto max-w-7xl flex items-center justify-between"}>
+                <h1 className={"text-4xl"}>Lorem Ipsum</h1>
+
+                <AppNavigation/>
+            </div>
+        </header>
+    )
 }
