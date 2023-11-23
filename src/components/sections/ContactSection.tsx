0156c3b5a7f5c3dd46a9e5911fd22670d3d8fe27
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 08:57:45 +0100

    add leaflet dependencies

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,17 +1,20 @@
 import SectionContainer from "@/components/sections/partials/SectionContainer";
 import SectionHeader from "@/components/sections/partials/SectionHeader";
 import ContactForm from "@/components/sections/contact/ContactForm";
+import "leaflet/dist/leaflet.css"
 
 export default function ContactSection() {
     return (
         <SectionContainer containerClassName={"grid grid-cols-2 gap-20 items-center"}>
-            <SectionHeader
-                subheading={"Lorem ipsum"}
-                heading={"Lorem ipsum dolor sit amet"}
-                description={"Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta."}
-            >
+            <div>
+                <SectionHeader
+                    subheading={"Lorem ipsum"}
+                    heading={"Lorem ipsum dolor sit amet"}
+                    description={"Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta."}
+                />
+
                 <ContactForm />
-            </SectionHeader>
+            </div>
         </SectionContainer>
     )
 }
--- src/components/sections/partials/SectionHeader.tsx
+++ src/components/sections/partials/SectionHeader.tsx
@@ -1,20 +1,15 @@
-import {ReactNode} from "react";
-
 interface SectionHeaderProps {
     subheading: string
     heading: string
     description: string
-    children?: ReactNode
 }
 
-export default function SectionHeader({subheading, heading, description, children}: SectionHeaderProps) {
+export default function SectionHeader({subheading, heading, description}: SectionHeaderProps) {
     return (
         <header className={"grid gap-4"}>
             <h3 className={"text-2xl"}>{subheading}</h3>
             <h2 className={"text-5xl"}>{heading}</h2>
             <p>{description}</p>
-
-            {children}
         </header>
     )
 }
