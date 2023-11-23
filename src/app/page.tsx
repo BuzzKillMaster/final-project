60c70c218adc9581013dcddc6595060e8435d995
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 07:52:03 +0100

    introduce fake training section until real training section is ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import AppHeader from "@/components/app/AppHeader";
 import AppFooter from "@/components/app/AppFooter";
 import HeroSection from "@/components/sections/HeroSection";
-import TrainingSection from "@/components/sections/TrainingSection";
 import ProfileSection from "@/components/sections/ProfileSection";
 import QualificationsSection from "@/components/sections/QualificationsSection";
 import ApproachSection from "@/components/sections/ApproachSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import GuideSection from "@/components/sections/GuideSection";
 import ContactSection from "@/components/sections/ContactSection";
+import FakeTrainingSection from "@/components/sections/FakeTrainingSection";
 
 export default function Home() {
     return (
@@ -16,7 +16,7 @@ export default function Home() {
 
             <main>
                 <HeroSection/>
-                <TrainingSection/>
+                <FakeTrainingSection/>
                 <ProfileSection/>
                 <QualificationsSection/>
                 <ApproachSection/>
---
+++ src/components/sections/FakeTrainingSection.tsx
@@ -0,0 +1,34 @@
+import SectionHeader from "@/components/sections/partials/SectionHeader";
+import FakeDynamicCard from "@/components/FakeDynamicCard";
+import SectionContainer from "@/components/sections/partials/SectionContainer";
+
+
+const data = Array(6).fill({
+    title: "Lorem ipsum dolor sit amet",
+    description: "Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta.",
+    imageUri: "https://fakeimg.pl/500/",
+    imageAlt: "temp",
+})
+
+export default function FakeTrainingSection() {
+    return (
+        <SectionContainer>
+            <SectionHeader subheading={"Lorem ipsum"} heading={"Lorem ipsum dolor sit amet"} description={"Vivamus quis semper tortor. Duis sapien sem, pretium non tristique nec, volutpat ut diam. Proin euismod mollis purus id porta."} />
+
+            <div className={"grid grid-cols-3 gap-20 mt-20"}>
+                {data.map((item, index) => (
+                    <FakeDynamicCard
+                        key={index}
+                        title={item.title}
+                        description={item.description}
+                        horizontal={false}
+                        imageUri={item.imageUri}
+                        imageAlt={item.imageAlt}
+                        imageHeight={200}
+                        imageWidth={200}
+                    />
+                ))}
+            </div>
+        </SectionContainer>
+    )
+}
