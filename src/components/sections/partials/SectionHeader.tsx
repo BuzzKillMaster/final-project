da28f911d823d9ea20c7c9f74978e813e089a058
Christian Pedersen <chris@buzzkill.dev>
2023-11-23 08:47:25 +0100

    allow section header to receive child components

--- src/components/sections/partials/SectionHeader.tsx
+++ src/components/sections/partials/SectionHeader.tsx
@@ -1,15 +1,20 @@
+import {ReactNode} from "react";
+
 interface SectionHeaderProps {
     subheading: string
     heading: string
     description: string
+    children?: ReactNode
 }
 
-export default function SectionHeader({subheading, heading, description}: SectionHeaderProps) {
+export default function SectionHeader({subheading, heading, description, children}: SectionHeaderProps) {
     return (
         <header className={"grid gap-4"}>
             <h3 className={"text-2xl"}>{subheading}</h3>
             <h2 className={"text-5xl"}>{heading}</h2>
             <p>{description}</p>
+
+            {children}
         </header>
     )
 }
