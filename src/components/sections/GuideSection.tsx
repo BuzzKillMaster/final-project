1d16b5074f1a1f3a08c3557fe9e3866c90298b8d
Christian Pedersen <chris@buzzkill.dev>
2023-11-28 12:04:19 +0100

    add preliminary guide section

--- src/components/sections/GuideSection.tsx
+++ src/components/sections/GuideSection.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import SectionContainer from "@/components/sections/partials/SectionContainer";
+import SectionHeader from "@/components/sections/partials/SectionHeader";
+import PrimaryButton from "@/components/PrimaryButton";
+
 export default function GuideSection() {
-    return (<></>)
+    return (
+        <SectionContainer containerClassName={"grid gap-12"}>
+            <SectionHeader subheading={"Lorem ipsum"} heading={"Lorem ipsum dolor sit amet"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique quis tempus id, scelerisque quis mauris."} />
+
+            <div>
+                <PrimaryButton type={"button"} text={"Click Me"} onClick={() => alert("Hello World!")} />
+            </div>
+        </SectionContainer>
+    )
 }
