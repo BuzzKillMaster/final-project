8d4654c355a88d753573328f2a4da3fd0d9217ec
Christian Pedersen <chris@buzzkill.dev>
2023-11-28 12:04:10 +0100

    create basic styling for primary button

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -6,7 +6,7 @@ interface PrimaryButtonProps {
 
 export default function PrimaryButton({type, text, onClick}: PrimaryButtonProps) {
     return (
-        <button type={type} onClick={onClick}>
+        <button type={type} onClick={onClick} className={"py-3 px-5 bg-neutral-950 text-neutral-50 rounded-full shadow font-semibold hover:bg-neutral-700"}>
             {text}
         </button>
     )
