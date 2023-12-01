1fdff42ca383bf4fff342377cdf4771295dbd0f4
Christian Pedersen <chris@buzzkill.dev>
2023-12-01 10:29:42 +0100

    Refactor HeroSection layout and add media query
    Removed the usage of SectionContainer in HeroSection.tsx and replaced it with a standard <section> element alongside tailwind CSS utilities for more direct layout control. Altered the grid dividing and item alignment in the same file. Added a media query to globals.css to provide a clip-path for the hero video on screens larger than 1024px width. Changes were made for cleaner code structure and improved design on larger screens.

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import SectionContainer from "@/components/sections/partials/SectionContainer";
 import SectionHeader from "@/components/sections/partials/SectionHeader";
 import PrimaryButton from "@/components/PrimaryButton";
 import HeroVideo from "@/components/sections/hero/HeroVideo";
@@ -10,8 +9,8 @@ export default function HeroSection() {
     const {hero} = useMockCMS()
 
     return (
-        <SectionContainer containerClassName={"grid md:grid-cols-2 gap-20 items-center"}>
-            <div className={"col-start-1 row-start-1 text-neutral-50 md:text-neutral-950 z-10"}>
+        <section className={"px-4 grid items-center lg:grid-cols-2"}>
+            <div className={"px-4 lg:px-0 col-start-1 row-start-1 text-neutral-50 md:text-neutral-950 z-10 justify-self-end lg:max-w-[40rem]"}>
                 <SectionHeader
                     subheading={hero.subheading}
                     heading={hero.heading}
@@ -21,9 +20,9 @@ export default function HeroSection() {
                 <PrimaryButton type={"button"} text={hero.button} onClick={() => {}} />
             </div>
 
-            <div className={"col-start-1 row-start-1 md:col-start-2"}>
+            <div className={"lg:-mr-4 col-start-1 row-start-1 lg:col-start-2 hero_video_container"}>
                 <HeroVideo src={hero.video} />
             </div>
-        </SectionContainer>
+        </section>
     )
 }
