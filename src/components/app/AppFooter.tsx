abf6291c1a546ebec7389b848e877bea8c87dadd
Christian Pedersen <chris@buzzkill.dev>
2023-12-04 08:00:28 +0100

    Remove FooterNavigation and update AppFooter styles
    The FooterNavigation component was removed. The styling within AppFooter was also updated to align with these changes.

--- src/components/app/AppFooter.tsx
+++ src/components/app/AppFooter.tsx
@@ -1,16 +1,12 @@
-import FooterNavigation from "@/components/app/navigation/FooterNavigation";
-
 export default function AppFooter() {
     return (
         <footer className={"py-12 px-4"}>
             <div className={"container mx-auto max-w-7xl"}>
                 <div className={"flex flex-col md:flex-row gap-12 items-center justify-between"}>
-                    <h2>Company Name ApS</h2>
-
-                    <FooterNavigation />
+                    <h2 className={"text-2xl font-semibold"}>Company Name ApS</h2>
                 </div>
 
-                <p className={"text-center mt-12"}>Copyright 2023 &copy; Company Name ApS. All rights reserved.</p>
+                <p className={"mt-4"}>Copyright 2023 &copy; Company Name ApS. All rights reserved.</p>
             </div>
         </footer>
     )
