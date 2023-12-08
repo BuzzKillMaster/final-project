43bef21689bac482f4d7aeb3e75f1ff361822f78
Christian Pedersen <chris@buzzkill.dev>
2023-12-08 11:33:30 +0100

    Enhance text presentation in CustomerTestimonial component
    Modified the 'author' className in CustomerTestimonial.tsx to include 'font-semibold', enhancing the visual emphasis on the author's name. This change improves readability by allowing users to easily distinguish the author from the content of the testimonial.

--- src/components/CustomerTestimonial.tsx
+++ src/components/CustomerTestimonial.tsx
@@ -11,7 +11,7 @@ export default function CustomerTestimonial({heading, testimonial, author}: Cust
         <div className={"grid gap-4 px-8 py-12 bg-neutral-50 bg-opacity-75 my-8 rounded shadow md:mr-[25%]"}>
             <h2 className={"text-4xl"}>{heading}</h2>
             <p className={"text-lg"}>{testimonial}</p>
-            <p className={"text-base"}>{author}</p>
+            <p className={"text-base font-semibold"}>{author}</p>
         </div>
     )
 }
