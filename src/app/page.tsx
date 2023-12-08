e113ea45839e4a031cee7f820af1d056335461b2
Christian Pedersen <chris@buzzkill.dev>
2023-12-08 08:06:18 +0100

    Adjust layout in AppFooter and fix indentation error
    The layout of the AppFooter component was revised to include the AppLogo. An indentation error in the back-to-top button of the Page component was also fixed. These changes improve the application's overall appearance and consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
             <AppFooter/>
 
-             <BackToTopButton/>
+            <BackToTopButton/>
         </>
     )
 }
--- src/components/app/AppFooter.tsx
+++ src/components/app/AppFooter.tsx
@@ -1,12 +1,12 @@
+import AppLogo from "@/components/app/AppLogo";
+
 export default function AppFooter() {
     return (
         <footer className={"py-12 px-4 shadow"}>
             <div className={"container mx-auto max-w-7xl"}>
-                <div className={"flex flex-col md:flex-row gap-12 items-center justify-between"}>
-                    <h2 className={"text-2xl font-semibold"}>Company Name ApS</h2>
-                </div>
+                <AppLogo/>
 
-                <p className={"mt-4"}>Copyright 2023 &copy; Company Name ApS. All rights reserved.</p>
+                <p className={"mt-8 opacity-75 font-medium text-center"}>Copyright 2023 &copy; Borreby Adfærdstræning. All rights reserved.</p>
             </div>
         </footer>
     )
