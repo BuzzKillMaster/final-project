abd2d8b9fc2925567c2eb566535ba31a7c6dc331
Christian Pedersen <chris@buzzkill.dev>
2023-12-19 07:50:18 +0100

    Add disabled property to PrimaryButton component
    The PrimaryButton component has been updated to include a 'disabled' property. This property is optional, but provides additional functionality for disabling the button under certain conditions, with a corresponding visual change in the button's appearance.

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -3,13 +3,14 @@ interface PrimaryButtonProps {
     text: string
     onClick: () => void
     secondary?: boolean
+    disabled?: boolean
 }
 
-export default function PrimaryButton({type, text, onClick, secondary = false}: PrimaryButtonProps) {
+export default function PrimaryButton({type, text, onClick, secondary = false, disabled = false}: PrimaryButtonProps) {
     const buttonColor = secondary ? "bg-[#B4D4CB] text-neutral-950 hover:brightness-75" : "bg-[#294E3D] text-neutral-50 hover:brightness-125"
 
     return (
-        <button type={type} onClick={onClick} className={buttonColor + " py-4 px-8 rounded-full shadow font-semibold"}>
+        <button type={type} onClick={onClick} className={buttonColor + " py-4 px-8 rounded-full shadow font-semibold disabled:bg-neutral-500 disabled:hover:brightness-100"} disabled={disabled}>
             {text}
         </button>
     )
