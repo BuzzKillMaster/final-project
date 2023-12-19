fe1fa646eea5e0799b607ab50970671db75d8471
Christian Pedersen <chris@buzzkill.dev>
2023-12-19 13:35:29 +0100

    Remove AppLogo from AppFooter component
    Removed 'AppLogo' from the AppFooter component. Also, the CSS class 'mt-8' was removed from the copyright Paragraph tag. This simplifies the footer design by eliminating the unnecessary logo and adjusting the space distribution.

--- src/components/app/AppFooter.tsx
+++ src/components/app/AppFooter.tsx
@@ -1,12 +1,8 @@
-import AppLogo from "@/components/app/AppLogo";
-
 export default function AppFooter() {
     return (
         <footer className={"py-12 px-4 bg-primary"}>
             <div className={"container mx-auto max-w-7xl"}>
-                <AppLogo/>
-
-                <p className={"mt-8 opacity-75 font-medium text-center"}>Copyright 2023 &copy; Borreby Adfærdstræning. Alle rettigheder reserveret.</p>
+                <p className={"opacity-75 font-medium text-center"}>Copyright 2023 &copy; Borreby Adfærdstræning. Alle rettigheder reserveret.</p>
             </div>
         </footer>
     )
