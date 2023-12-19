089f9e25a2b22ddf314731ed0c895cb703d38bb2
Christian Pedersen <chris@buzzkill.dev>
2023-12-19 13:29:17 +0100

    Add new asset host to next.config.js
    Added 'assets.tina.io' with https protocol to the list of default asset hosts in next.config.js. This config file determines where the application looks for its static resources. The change allows the application to fetch assets from this additional host.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
                 hostname: 'fakeimg.pl',
             },
             {
+                protocol: 'https',
+                hostname: 'assets.tina.io'
+            },
+            {
+                protocol: 'https',
+                hostname: 'borreby.vercel.app'
+            },
+            {
                 protocol: 'http',
                 hostname: 'localhost'
             }
