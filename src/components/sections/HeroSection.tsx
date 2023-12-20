6361e8257780184f276417b775c57b336138ebfa
Christian Pedersen <chris@buzzkill.dev>
2023-12-20 07:57:30 +0100

    Update HeroSection component styles
    Refined the presentation of HeroSection component by changing padding, text and background colors in classes. These updates aim to improve the visual appeal and ensure better readability of elements within this section.

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -20,20 +20,25 @@ export default function HeroSection() {
     if (!hero) return null
 
     return (
-        <section id={"hero_section"} className={"px-4 grid items-center lg:grid-cols-2 shadow"}>
-            <div className={"px-4 lg:px-0 col-start-1 row-start-1 text-neutral-50 md:text-neutral-950 z-10 justify-self-end w-full lg:max-w-[40rem]"}>
+        <section id={"hero_section"} className={"lg:px-4 grid items-center lg:grid-cols-2 shadow relative"}>
+            <div
+                className={"py-12 lg:py-0 px-4 lg:px-0 col-start-1 row-start-1 text-neutral-50 lg:text-neutral-950 relative z-10 justify-self-end w-full lg:max-w-[40rem]"}>
                 <SectionHeader
                     subheading={hero.subheading}
                     heading={hero.heading}
                     description={hero.description}
                 />
 
-                <PrimaryButton type={"button"} text={hero.button} onClick={() => {}} />
+                <PrimaryButton type={"button"} text={hero.button} onClick={() => {
+                }}/>
             </div>
 
+
             <div className={"lg:-mr-4 col-start-1 row-start-1 lg:col-start-2 hero_video_container"}>
-                <HeroVideo src={hero.hero_video} />
+                <HeroVideo src={hero.hero_video}/>
             </div>
+
+            <div className={"lg:hidden absolute inset-0 bg-neutral-800 backdrop-blur-sm bg-opacity-75"}></div>
         </section>
     )
 }
