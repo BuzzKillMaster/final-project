3167f2ac5e4092c99e33c615a563f6783769ab49
Christian Pedersen <chris@buzzkill.dev>
2023-12-20 07:42:19 +0100

    Update color schemes in multiple components
    Adjusted color schemes and styles across different components, including SectionContainer, SectionHeader, global.css, and AppFooter. Changes to the background-color, text-color, and other CSS styles aim to improve overall design coherence and user visual experience.

--- src/components/app/AppFooter.tsx
+++ src/components/app/AppFooter.tsx
@@ -1,6 +1,6 @@
 export default function AppFooter() {
     return (
-        <footer className={"py-12 px-4 bg-primary"}>
+        <footer className={"py-12 px-4"}>
             <div className={"container mx-auto max-w-7xl"}>
                 <p className={"opacity-75 font-medium text-center"}>Copyright 2023 &copy; Borreby Adfærdstræning. Alle rettigheder reserveret.</p>
             </div>
--- src/components/sections/partials/SectionHeader.tsx
+++ src/components/sections/partials/SectionHeader.tsx
@@ -12,7 +12,7 @@ interface SectionHeaderProps {
 export default function SectionHeader({subheading, heading, description}: SectionHeaderProps) {
     return (
         <header className={"whitespace-pre-line relative z-10"}>
-            <h3 className={quicksand.className + " text-2xl font-medium"}>{subheading}</h3>
+            <h3 className={quicksand.className + " text-2xl font-medium text-[#94A79E]"}>{subheading}</h3>
             <h2 className={quicksand.className + " text-5xl font-semibold mt-4 mb-6 leading-snug"}>{heading}</h2>
 
             <div className={"max-w-lg grid gap-4 mb-8"}>
